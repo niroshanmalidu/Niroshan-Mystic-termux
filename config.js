c4fa62aa5ce60c07e44f7de07e22cd5fbd871242
niroshanmalidu <123802907+niroshanmalidu@users.noreply.github.com>
2023-02-04 15:43:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,20 +7,9 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 import moment from 'moment-timezone' 
 
-global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['50499698072', '💫 Mystic - Collaborator 1 💫', true],
-  ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
-  ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
-  ['573225236629', '💫 Mystic - Collaborator 4 💫', true],
-  ['593959425714', '💫 Mystic - Collaborator 5 💫', true],
-  ['50246028932', '💫 Mystic - Collaborator 6 💫', true],  
-  ['5212411719888', '💫 Mystic - Collaborator 7 💫', true],	
-  ['593968585283'],
-  ['5219993404349'],
-  ['5219991402134'],	
-  ['5219996125657']
-] 
+global.owner = [+94714184446] 	
+
+
 
 global.suittag = ['5219993404349'] 
 global.prems = ['51995386439'] 
